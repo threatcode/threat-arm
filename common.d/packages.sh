threatcode/threat-arm

commit 0d556a7026ab3bb928f28143583b6020785301ca

common: tftp->tftp-hpa
Reported by Im_Soap, the images weren't building (not seeing this on the
jenkins server), looking into their log, it's because it tries to pull
in tftp and not the replacement tftp-hpa since tftp went away.

Closes #311.

common.d/packages.sh
@@ -10,7 +10,7 @@ minimal_pkgs="ca-certificates haveged iw network-manager parted sudo wpasupplica
 # This is the list of minimal common packages
 common_min_pkgs="$minimal_pkgs apt-transport-https command-not-found \
 fontconfig ifupdown kali-defaults kali-tweaks man-db mlocate net-tools \
-netcat-traditional pciutils psmisc rfkill screen snmp snmpd tftp tmux unrar \
+netcat-traditional pciutils psmisc rfkill screen snmp snmpd tftp-hpa tmux unrar \
 usbutils vim wireless-regdb zerofree zsh zsh-autosuggestions \
 zsh-syntax-highlighting"
 

commit 0df96b4f6169dc3fbc5e5a3f5b4637277d16b2d2

common: add missing tftp-hpa
Fixes: 0d556a7026ab3bb928f28143583b6020785301ca

common.d/packages.sh
@@ -17,7 +17,7 @@ zsh-syntax-highlighting"
 # This is the list of common packages
 common_pkgs="$minimal_pkgs apt-transport-https dialog \
 ifupdown inxi kali-linux-core kali-linux-firmware libnss-systemd man-db mlocate net-tools \
-network-manager pciutils psmisc rfkill screen snmp snmpd tftp \
+network-manager pciutils psmisc rfkill screen snmp snmpd tftp-hpa \
 triggerhappy usbutils whiptail zerofree"
 
 services="apache2 atftpd openvpn ssh tightvncserver"