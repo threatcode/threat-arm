293c3fd710f88a3462703bf731957232033a0469
Steev Klimaszewski <steev@kali.org>
2023-10-09 12:39:48 -0500

    common: Add polkitd-pkla package and fix directory
    On amd64 systems, polkitd-pkla is installed as a part of suggests from
    systemd, but that doesn't occur on the arm images, so make sure that the
    package is installed on all images.  Additionally, install the
    networkmanager pkla file to the correct folder.  Thanks to @arszilla for
    pointing this out.

--- common.d/base_image.sh
+++ common.d/base_image.sh
@@ -120,7 +120,7 @@ systemctl enable ssh
 
 status_stage3 'Allow users to use NetworkManager over SSH'
 # Ensure the localauhority directory exists before trying to install into it.
-mkdir -p /var/lib/polkit-1/localauthority
+mkdir -p /var/lib/polkit-1/localauthority/50-local.d
 install -m644 /bsp/polkit/10-NetworkManager.pkla /var/lib/polkit-1/localauthority/50-local.d
 
 status_stage3 'Copy script growpart'
